172370fc0f0764162b5f23b70caad2c59a49e70e
ZhuravlevMax <l9sik3@gmail.com>
2022-11-04 09:44:15 +0300

    Create project

---
+++ MinskMetroSchedule/MinskMetroSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinskMetroSchedule
+//
+//  Created by Максим Журавлев on 4.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
