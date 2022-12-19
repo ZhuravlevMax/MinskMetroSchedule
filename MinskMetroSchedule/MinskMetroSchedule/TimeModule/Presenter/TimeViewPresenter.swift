801a358a6f129334bde0933931f486d25f8ba8fa
ZhuravlevMax <l9sik3@gmail.com>
2022-12-19 14:53:49 +0300

    Fix bug with timeTableView

--- MinskMetroSchedule/MinskMetroSchedule/TimeModule/Presenter/TimeViewPresenter.swift
+++ MinskMetroSchedule/MinskMetroSchedule/TimeModule/Presenter/TimeViewPresenter.swift
@@ -72,29 +72,29 @@ class TimeViewPresenter: TimeViewPresenterProtocol {
         
         guard let direction = FireBaseFieldsEnum(rawValue: toStationName),
               
-              let stations = UserDefaults.standard.object(forKey: "\(UserDefaultsKeysEnum.allDayData)") as? [String:Any],
+                let stations = UserDefaults.standard.object(forKey: "\(UserDefaultsKeysEnum.allDayData)") as? [String:Any],
               let station = stations[stationName] as? [String:Any],
-        let timeSheet = station["\(direction)"] as? [Int] else {return}
-
-                var currentTimeFromStartDay = Int(Date().timeIntervalSince1970) - Int(Calendar.current.startOfDay(for: Date()).timeIntervalSince1970)
-                
-                // Здесь добавил условие, чтобы исправить баг со временем, когда пользователь смотрит расписание после 00:00
-                if currentTimeFromStartDay < 1500 {
-                    currentTimeFromStartDay += 86400
-                }
-                
-                let nextTime = timeSheet.first { $0 > currentTimeFromStartDay }
-                
-                guard var nextTime else {return}
-                
-                //Здесь условие, для того чтобы у пользователя отображалось время не 24:00, а 00:00
-                if currentTimeFromStartDay > 86400 {
-                    nextTime -= 86400
-                }
-                
-                let formattedString = nextTime.decoderDt(format: "HH:mm")
-                self.view?.setNextTimeLabel(nextTime: formattedString)
-
+              let timeSheet = station["\(direction)"] as? [Int] else {return}
+        
+        var currentTimeFromStartDay = Int(Date().timeIntervalSince1970) - Int(Calendar.current.startOfDay(for: Date()).timeIntervalSince1970)
+        
+        // Здесь добавил условие, чтобы исправить баг со временем, когда пользователь смотрит расписание после 00:00
+        if currentTimeFromStartDay < 1500 {
+            currentTimeFromStartDay += 86400
+        }
+        
+        let nextTime = timeSheet.first { $0 > currentTimeFromStartDay }
+        
+        guard var nextTime else {return}
+        
+        //Здесь условие, для того чтобы у пользователя отображалось время не 24:00, а 00:00
+        if currentTimeFromStartDay > 86400 {
+            nextTime -= 86400
+        }
+        
+        let formattedString = nextTime.decoderDt(format: "HH:mm")
+        self.view?.setNextTimeLabel(nextTime: formattedString)
+        
     }
     
     func configureTimeSheetTableViewCell(indexPath: IndexPath,
@@ -108,43 +108,43 @@ class TimeViewPresenter: TimeViewPresenterProtocol {
               let dayOfWeek = UserDefaults.standard.string(forKey: "\(UserDefaultsKeysEnum.dayOfWeek)"),
               let timeSheet = station["\(direction)"] as? [Int]
         else {return}
-
-                let currentTimeFromStartDay = Int(Date().timeIntervalSince1970) - Int(Calendar.current.startOfDay(for: Date()).timeIntervalSince1970)
-                
-                let hoursArrayNext = timeSheet.filter {$0 > currentTimeFromStartDay}
-                let hoursArray = hoursArrayNext.map {$0 / 3600}
-                let hours = Array(Set(hoursArray)).sorted { $0 < $1 }
-                var hourModify: [Int] = []
-                
-                for hour in hours {
-                    
-                    if hour > 23 {
-                        let newHour = hour - 24
-                        hourModify.append(newHour)
-                    } else {
-                        hourModify.append(hour)
-                    }
-                }
-                
-                //let hourModifyString = hourModify.map { String($0) }
-                let minutesArray = timeSheet.map {($0 % 3600) / 60}
-                // print(minutesArray)
-                
-                var minutesAll: [[Int]] = []
-                
-                for _ in hoursArray {
-                    var minutes = hoursArrayNext.filter { return $0 / 3600 == hours[indexPath.row]}
-                    minutes = minutes.map {($0 % 3600) / 60}
-                    
-                    minutesAll.append(minutes)
-                }
-                
-                let minutesAllString = minutesAll[indexPath.row].map { (String(format: "%02d", arguments: [$0]))}
-                let minutesString = minutesAllString.joined(separator:" ")
-                
-                cell.configureCell(hourValue: "\(String(format: "%02d", arguments: [hourModify[indexPath.row]])):",
-                                   minutesValue: minutesString)
-
+        
+        let currentTimeFromStartDay = Int(Date().timeIntervalSince1970) - Int(Calendar.current.startOfDay(for: Date()).timeIntervalSince1970)
+        
+        let hoursArrayNext = timeSheet.filter {$0 > currentTimeFromStartDay}
+        let hoursArray = hoursArrayNext.map {$0 / 3600}
+        let hours = Array(Set(hoursArray)).sorted { $0 < $1 }
+        var hourModify: [Int] = []
+        
+        for hour in hours {
+            
+            if hour > 23 {
+                let newHour = hour - 24
+                hourModify.append(newHour)
+            } else {
+                hourModify.append(hour)
+            }
+        }
+        
+        //let hourModifyString = hourModify.map { String($0) }
+        let minutesArray = timeSheet.map {($0 % 3600) / 60}
+        // print(minutesArray)
+        
+        var minutesAll: [[Int]] = []
+        
+        for _ in hoursArray {
+            var minutes = hoursArrayNext.filter { return $0 / 3600 == hours[indexPath.row]}
+            minutes = minutes.map {($0 % 3600) / 60}
+            
+            minutesAll.append(minutes)
+        }
+        
+        let minutesAllString = minutesAll[indexPath.row].map { (String(format: "%02d", arguments: [$0]))}
+        let minutesString = minutesAllString.joined(separator:" ")
+        
+        cell.configureCell(hourValue: "\(String(format: "%02d", arguments: [hourModify[indexPath.row]])):",
+                           minutesValue: minutesString)
+        
         
         
     }
